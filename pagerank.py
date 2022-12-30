385dc6791a827730dbbd1be82cff6066c333fa2e
Damian Piorun <damianpiorun@Damians-MacBook-Pro.local>
2022-12-30 19:52:45 +0100

    implement pagerank

--- pagerank.py
+++ pagerank.py
@@ -2,6 +2,7 @@ import os
 import random
 import re
 import sys
+import copy
 
 DAMPING = 0.85
 SAMPLES = 10000
@@ -86,7 +87,34 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    track_visited = {}
+    transition_weights = {}
+    counter = 0
+    pages = list(corpus.keys())
+
+    for page_name in pages:
+        track_visited[page_name] = 0
+        
+        model = transition_model(corpus, page_name, damping_factor)
+        weights = []
+        for page_name_model in pages:
+            weights.append(model[page_name_model])
+        
+        transition_weights[page_name] = weights
+
+
+    sample_page = random.choice(pages)
+    
+    while counter < n:
+        counter = counter + 1
+        track_visited.update({sample_page: track_visited[sample_page] + 1})
+        sample_page = random.choices(pages, weights=transition_weights[sample_page])[0]
+    
+    retval = {}
+    for page_name in pages:
+        retval[page_name] = track_visited[page_name] / n
+
+    return retval
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -98,7 +126,39 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    retval = {}
+    previous_PR = {}    
+    linked_pages = {}
+    pages = list(corpus.keys())
+
+    for page_name in pages:
+        retval[page_name] = 1 / len(corpus)
+        previous_PR[page_name] = 0
+        linked_pages[page_name] = set()
+
+    for page in corpus:
+        for link in corpus[page]:
+            linked_pages[link].add(page)
+
+    while not is_converged(previous_PR, retval):
+        previous_PR = copy.deepcopy(retval)
+        for page_name in pages:
+            incoming_pages_probability = 0
+            for link in linked_pages[page_name]:
+                incoming_pages_probability = incoming_pages_probability + (previous_PR[link] / len(corpus[link]))
+
+            retval[page_name] = ((1 - damping_factor) / len(corpus)) + (damping_factor * incoming_pages_probability)
+
+    return retval
+
+def is_converged(previous: dict, current: dict, accuracy=0.001) -> bool:
+    if previous.keys() != current.keys():
+        raise ValueError("Cannot calculate accuracy for dictionaries with incompatible keys")
+    
+    for page_name in previous:
+        if abs(previous[page_name] - current[page_name]) > accuracy:
+            return False
+    return True
 
 
 if __name__ == "__main__":
--- test_pagerank.py
+++ test_pagerank.py
@@ -14,11 +14,11 @@ from pagerank import *
             },
             "1.html",
             0.85,
-            {
-                "1.html": pytest.approx(0.05, 0.0001),
-                "2.html": pytest.approx(0.475, 0.0001),
-                "3.html": pytest.approx(0.475, 0.0001)
-            }
+            pytest.approx({
+                "1.html": 0.05,
+                "2.html": 0.475,
+                "3.html": 0.475
+            }, 0.001)
         ),
         (
             {
@@ -46,3 +46,84 @@ def test_transition_model(corpus, page, damping_factor, expected):
 
     assert output == expected
 
+
+
+@pytest.mark.parametrize(
+    "corpus,damping_factor,n,expected",
+    [
+        (
+            {
+                "1.html": {"2.html", "3.html"}, 
+                "2.html": {"3.html"}, 
+                "3.html": {"2.html"}
+            },
+            0.85,
+            10000,
+            pytest.approx({
+                "1.html": 0.05,
+                "2.html": 0.475,
+                "3.html": 0.475
+            }, abs=0.005)
+        ),
+        (
+            {
+                "1.html": {}, 
+                "2.html": {}, 
+                "3.html": {}
+            },
+            0.85,
+            10000,
+            pytest.approx({
+                "1.html": 0.333,
+                "2.html": 0.333,
+                "3.html": 0.333
+            }, abs=0.01)
+        )
+    ]
+)
+def test_sample_pagerank(corpus, damping_factor, n, expected):
+    """
+    It should return estimated PageRank for each page.
+    """    
+    output = sample_pagerank(corpus, damping_factor, n)
+    
+    assert output == expected
+    
+    sum = 0
+    for page_rank in list(output.keys()):
+        sum = sum + output[page_rank]
+    assert sum == 1 # values in output should sum up to 1
+
+
+@pytest.mark.parametrize(
+    "corpus,damping_factor,expected",
+    [
+        (
+            {
+                "1.html": {"2.html", "3.html"}, 
+                "2.html": {"3.html"}, 
+                "3.html": {"2.html"}
+            },
+            0.85,
+            pytest.approx({
+                "1.html": 0.05,
+                "2.html": 0.475,
+                "3.html": 0.475
+            }, 0.001)
+        )
+    ]
+)
+def test_iterate_pagerank(corpus, damping_factor, expected):
+    """
+    It should return each page's PageRank accurate to within 0.001.
+    """    
+    output = iterate_pagerank(corpus, damping_factor)
+    
+    assert output == expected
+    
+    sum = 0
+    for page_rank in list(output.keys()):
+        sum = sum + output[page_rank]
+    assert sum == 1 # values in output should sum up to 1
+
+    
